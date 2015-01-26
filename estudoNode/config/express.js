67ae0cdb1d9d05668cd3bd2554c69a8c1c19e4f0
torto <tortoyoyo@gmail.com>
2015-01-26 17:59:55 -0200

    primeiro commit

---
+++ estudoNode/config/express.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var home = require('../app/routes/home');
+
+module.exports = function() {
+	var app = express();
+
+	app.set('port', 3000);
+
+	app.use(express.static('./public'));
+	//app.use(app.router);
+
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
+	
+	home(app);
+
+	return app;
+}
